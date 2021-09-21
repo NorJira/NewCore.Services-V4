b6023622d0d88b0d5ef6d8d9a5dd8cedff22f803
norjira <norjira@gmail.com>
2021-09-21 11:41:14 +0700

    Add customer services/repository

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NewCore.Services
-{
-    public class Class1
-    {
-    }
-}
---
+++ CustomerServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NewCore.Data;
+
+namespace NewCore.Services
+{
+    public class CustomerServices
+    {
+        public CustomerServices()
+        {
+        }
+    }
+}
